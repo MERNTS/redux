619d2c4ab3958cae27d3e6e5d15530a9f8d10d42
aiXpertLab <aiXpertLab@gmail.com>
2024-05-19 19:46:55 -0400

    V1.NoRedux

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './assets/App.css'
 import Counter from './components/Counter'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
