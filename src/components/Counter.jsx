791578c7f639691b58fbaaa8b578d1b850632c56
aiXpertLab <aiXpertLab@gmail.com>
2024-05-19 20:27:27 -0400

    v1.noredux

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,31 @@
 import { useState } from 'react'
 
-const Counter = () => {
+const Counter = ({ counter, increment, decrement }) => {
   const [count, setCount] = useState(99)
 
+  const handleIncrement=()=>{
+    setCount((prevCount) => prevCount +2)
+  }
+
+  const handleDecrement=()=>{
+    setCount((prevCount)=>Math.max(prevCount-1),0)
+  }
+
+
   return (
-    <button onClick={() => setCount((count) => count + 1)}>
-      count is {count}
-    </button>
+    <div>
+        <p className="counter_title">Counter: {count}</p>
+        <button className="button" onClick={handleIncrement}>
+            Increment
+        </button>
+
+        <button className="button" onClick={handleDecrement}>
+            Decrement
+        </button>
+    </div>
+//     <button onClick={() => setCount((count) => count + 1)}>
+//       count is {count}
+//     </button>
   )
 }
 
